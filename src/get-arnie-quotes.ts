fbe3519df68cf30796f9edfc86c33943a28747fc
George Zhang <george.zh.js@gmail.com>
2022-03-28 10:20:57 +1100

    Test: implement "get-arine-quotes" function

--- src/get-arnie-quotes.ts
+++ src/get-arnie-quotes.ts
@@ -1,10 +1,36 @@
 import { httpGet } from './mock-http-interface';
 
-// TODO define this type properly
-type TResult = any;
+type FailureResult = {
+  FAILURE: 'Your request has been terminated',
+};
+
+type SuccessResult = {
+  [key: string]: string;
+};
+
+type TResult = SuccessResult | FailureResult;
+
+type TResponse = {
+  status: number;
+  body: string;
+}
+
+const RES_CODE_KEY_MAPPING: { [key: string]: string} = {
+  '200': 'Arnie Quote',
+  default: 'FAILURE'
+};
+
+function serialiseResponse(response: TResponse): TResult {
+  const { status, body } = response;
+  const statusCodeInString = status === 200 ? String(status) : 'default';
+  const resultKey: string = RES_CODE_KEY_MAPPING[statusCodeInString];
+  const bodyInJson = JSON.parse(body);
+  return { [resultKey]: bodyInJson.message};
+}
 
 export const getArnieQuotes = async (urls : string[]) : Promise<TResult[]> => {
-  // TODO: Implement this function.
+  const requests = urls.map(httpGet)
+  const responses = await Promise.all(requests);
   
-  return [];
+  return responses.map(serialiseResponse);
 };
